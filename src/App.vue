4b8b6d4b658b5b398c11e4c72afbc3120c5cdedc
Aman <rumman.djuice1@gmail.com>
2019-01-31 22:08:28 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-
+                <p>Using local filter "to-upperCasae":{{name | to-upperCase }}</p>
+                <p>Using global filter "to-lowerCasae": {{name | to-upperCase | to-lowerCase}}</p>
             </div>
         </div>
     </div>
@@ -11,6 +12,16 @@
 
 <script>
     export default {
+        data(){
+            return{
+                name:'Hello',
+            };
+        },
+        filters:{
+            'to-upperCase'(value){
+                return value.toUpperCase();
+            }
+        }
     }
 </script>
 
